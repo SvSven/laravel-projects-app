7a5b2e1a56239dbce43a91283f17d3a706146776
Sven van Steenis <sven.vsteenis@gmail.com>
2019-02-04 22:43:47 +0100

    swap bootstrap navbar layout for bulma navbar layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,43 +21,47 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+        <nav class="navbar" role="navigation" aria-label="main navigation">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                <div class="navbar-brand">
+                    <a class="navbar-item" href="{{ url('/') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                    <a role="button" class="navbar-burger burger" aria-label="{{ __('Toggle navigation') }}" aria-expanded="false" data-target="main-menu">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
 
-                    </ul>
+                <div id="main-menu" class="navbar-menu">
+                    <div class="navbar-start">
+                        <a class="navbar-item" href="{{ route('home') }}">
+                            Home
+                        </a>
+                    </div>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    <div class="navbar-end">
+                        <div class="navbar-item {{ Auth::check() ? 'has-dropdown is-hoverable' : '' }}">
+                            @guest
+                            <div class="buttons">
+                                <a href="{{ route('login') }}" class="button is-light">{{ _('Login') }}</a>
+
+                                @if (Route::has('register'))
+                                    <a href="{{ route('register') }}" class="button is-primary">{{ _('Register') }}</a>
+                                @endif
+                            </div>
+                            @else
+
+                                <a class="navbar-link">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                <div class="navbar-dropdown">
+                                    <a class="navbar-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                        document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
@@ -65,9 +69,10 @@
                                         @csrf
                                     </form>
                                 </div>
-                            </li>
-                        @endguest
-                    </ul>
+
+                            @endguest
+                        </div>
+                    </div>
                 </div>
             </div>
         </nav>
